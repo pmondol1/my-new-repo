c1107b0baaaceb4f20c86e03033e55d9a7c39608
Parmita Modi <parmita.mondol@gmail.com>
2025-10-14 19:22:28 -0500

    Save and stage all changes

--- app.py
+++ app.py
@@ -31,13 +31,13 @@ st.plotly_chart(fig_price)
 # Scatterplots
 st.header('Scatterplots')
 fig_scatter1 = px.scatter(df, x='odometer', y='price', title='Price vs Odometer')
-st.plotly_chart(fig_scatter)
+st.plotly_chart(fig_scatter1)
 
 # Checkbox to filter by condition
 if st.checkbox("Show only vehicles in excellent condition"):
-    filtered_df = df[df["condition"] == "excellent"]
-    fig_filtered = px.histogram(filtered_df, x="price", title="Price Distribution (Excellent Condition)")
-    st.plotly_chart(fig_filtered)
+filtered_df = df[df["condition"] == "excellent"]
+fig_filtered = px.histogram(filtered_df, x="price", title="Price Distribution (Excellent Condition)")
+st.plotly_chart(fig_filtered)
 
 
 st.markdown("---")
